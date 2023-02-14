834bd3b76bb5a4b397f4e7639ad5601f79d795f7
Cryptogoldenergy <cfrylinck1997@gmail.com>
2023-02-14 23:25:25 +0200

    Added blue to oxygen color

--- client/src/components/bases/resourceList.js
+++ client/src/components/bases/resourceList.js
@@ -16,7 +16,7 @@ const ResourceList = ({
                 <ResourceComponent color="#833415" name={"Lumber"} available={resources.lumber} production={1000} />
                 <ResourceComponent color="#61699a" name={"Water"} available={resources.water} production={1000} />
                 <ResourceComponent color="#187661" name={"Food"} available={resources.food} production={1000} />
-                <ResourceComponent color="#aeb8d1" name={"Oxygen"} available={resources.oxygen} production={1000} />
+                <ResourceComponent color="#aeb8e4" name={"Oxygen"} available={resources.oxygen} production={1000} />
                 <ResourceComponent color="#00c1ff" name={"Crystal_Blue"} available={resources.crystal_Blue} production={1000} />
                 <ResourceComponent color="#ff0000" name={"Crystal_Red"} available={resources.crystal_Red} production={1000} />
                 <ResourceComponent color="#a3a034" name={"Gold"} available={resources.gold} production={1000} />
